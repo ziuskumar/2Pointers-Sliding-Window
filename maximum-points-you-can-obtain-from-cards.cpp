c6b2fcef00cafce621a8cb899d1f20849772e06e
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2026-03-21 10:45:02 +0530

    Add maxScore function to calculate max card points
    Implement maxScore function to calculate maximum points from cardPoints.

---
+++ maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int maxl = 0;
+        int n = cardPoints.size();
+        int lsum = 0 , rsum = 0;
+
+        for(int i=0; i<k ;i++){
+            lsum = lsum + cardPoints[i];
+            maxl = lsum;
+        }
+
+        int ridx = n-1;
+
+        for(int i=k-1; i>=0; i--){
+            lsum = lsum - cardPoints[i];
+            rsum = rsum + cardPoints[ridx];
+
+            ridx--;
+            maxl = max(maxl, (rsum+lsum));
+        }
+        return maxl;
+    }
+};
