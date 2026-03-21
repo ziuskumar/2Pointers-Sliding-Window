843bb95bfe85b550f003a0872ce527fd17cc1b78
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2026-03-21 12:32:36 +0530

    Implement longestOnes function in max-consecutive-ones-iii.cpp

---
+++ max-consecutive-ones-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l = 0 , r = 0, maxlen = 0, n = nums.size(), zeros = 0;
+
+        while(r < n){
+
+            if(nums[r] == 0){
+                zeros++;
+            }
+
+            if(zeros > k){
+                if(nums[l] == 0){
+                    zeros--;
+                   
+                }
+                l++;
+            }
+                
+                    int len = r - l + 1;
+                    maxlen = max(len, maxlen);
+
+                 r++;
+        }
+        return maxlen;
+    }
+};
+
